c558d2545f2a88486c19c07819e3bea280d2fb28
Melissa Winstanley <aimlessrose@gmail.com>
2024-01-25 18:09:10 -0800

    Add slides for proposals and extra slides for lecture 4

--- 04/sample/utils/histogram.py
+++
@@ -1,8 +0,0 @@
-def hist(dictionary):
-    result = {}
-    for v in dictionary.values():
-        if v in result:
-            result[v] += 1
-        else:
-            result[v] = 1
-    return result
--- 05/imports/wildcard_imports.py
+++
@@ -1,8 +0,0 @@
-from math import *
-
-print(cos(2*pi))
-
-from my_math.math_fns import *
-
-print(cos(2*pi))
-
